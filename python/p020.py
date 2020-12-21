19ac20239030cfa6255409e967d30979266aead7
AlkisPis <a_pis@otenet.gr>
2020-12-21 20:23:53 +0200

    Update p020.py
    def compute():
    	# Improved - The string needs to be computed just once
    	s = str(math.factorial(100))
    	return sum(int(c) for c in s)
    
    if __name__ == "__main__":
    	print(compute())

--- python/p020.py
+++ python/p020.py
@@ -11,10 +11,15 @@ import math
 
 # We do a straightforward computation thanks to Python's built-in arbitrary precision integer type.
 def compute():
+	'''
+	Original (Inefficient)
 	n = math.factorial(100)
-	ans = sum(int(c) for c in str(n))
+	ans = sum(int(c) for c in str(n)) # This computes 'str(n)' 100 times!!!
 	return str(ans)
-
+	'''
+	# Improved - The string needs to be computed just once
+	s = str(math.factorial(100))
+	return sum(int(c) for c in s)
 
 if __name__ == "__main__":
 	print(compute())
